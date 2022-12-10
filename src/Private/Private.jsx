5fb16e0dd959acdfa8c3a363eda64e4dd04c7cd9
allsgit <allanseu@gmail.com>
2022-12-10 17:55:30 +0100

    vercel2

--- src/Private/Private.jsx
+++ src/Private/Private.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { useContext } from 'react';
-import { UserContext } from './Context/userContext';
+import { UserContext } from '../context/userContext';
 
 export default function Private() {
   const { currentUser } = useContext(UserContext);
