d13f893b54e14ff6c8ae1c79d04b0ac255bb22ee
allsgit <allanseu@gmail.com>
2022-12-11 22:00:00 +0100

    admin log trashed

--- src/components/Advices.jsx
+++ src/components/Advices.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 const AdvWrapper = styled.div`
-    position: absolute;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-p{
-
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  p {
     width: auto;
     height: auto;
     background-color: white;
     opacity: 1;
     z-index: 10000;
-}
+  }
 `;
 
 export default function Advices() {
   return (
     <AdvWrapper>
-      <p>log admin : admin@food.com - password : aaaaaa</p>
-      <p>log user : foo@foo.com - password : aaaaaa   --- Or --- create new user </p>
+      <p>log user : foo@foo.com - password : aaaaaa --- Or --- create new user </p>
     </AdvWrapper>
   );
 }
